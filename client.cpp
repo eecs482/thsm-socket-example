70be7de0845acbcfcc2e753c910447bf8002a173
Thomas Smith <thsm@umich.edu>
2021-11-19 14:57:07 -0500

    Add solution

--- client.cpp
+++ client.cpp
@@ -17,10 +17,44 @@ int main() {
     std::string data_to_send = "Hello, world!\n";
     uint16_t server_port = 12345;
 
-    // TODO: Send data_to_send to the server.
+    int connection = socket(AF_INET, SOCK_STREAM, 0);
+    if (connection == -1) {
+        std::cerr << "Error getting socket\n";
+        return 1;
+    }
+
+    hostent *host = gethostbyname("localhost");
+    if (!host) {
+        std::cerr << "Error resolving host\n";
+        return 1;
+    }
+
+    sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    memcpy(&server_addr.sin_addr, host->h_addr, host->h_length);
+    server_addr.sin_port = htons(server_port);
+
+    if (connect(connection, (sockaddr *) &server_addr, sizeof(server_addr)) == -1) {
+        std::cerr << "Error connecting to server\n";
+        return 1;
+    }
+
+    if (send(connection, data_to_send.c_str(), data_to_send.length() + 1, 0) == -1) {
+        std::cerr << "Error sending data\n";
+        return 1;
+    }
 
     std::string ack = "ACK";
-    // TODO: Receive ACK
+    std::array<char, 3> buf;
+    if (recv(connection, buf.data(), buf.size(), MSG_WAITALL) != buf.size()) {
+        std::cerr << "Error receving ACK\n";
+        return 1;
+    }
+
+    assert(std::string(buf.data(), buf.size()) == ack);
+    std::cout << "Got ACK from server\n";
+
+    close(connection);
 
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -15,10 +15,58 @@ int main() {
 
     uint16_t server_port = 12345;
     
-    // TODO: Receive data from the client and print it.
+    int server = socket(AF_INET, SOCK_STREAM, 0);
+    if (server == -1) {
+        std::cerr << "Error getting socket\n";
+        return 1;
+    }
+    int yes = 1;
+	if (setsockopt(server, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1) {
+        std::cerr << "Error setting socket options\n";
+        return 1;
+    }
 
-    std::string ack = "ACK";
-    // TODO: Send ACK
+    sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(server_port);
+
+    if (bind(server, (sockaddr *) &server_addr, sizeof(server_addr)) == -1) {
+        std::cerr << "Error binding to port\n";
+        return 1;
+    }
+
+    if (listen(server, 5) == -1) {
+        std::cerr << "Error listening\n";
+        return 1;
+    }
+
+    int connection = accept(server, nullptr, nullptr);
+    if (connection == -1) {
+        std::cerr << "Error accepting connection\n";
+        return 1;
+    }
+
+    while (true) {
+        std::array<char, 100> buf;
+        int len = recv(connection, buf.data(), buf.size(), 0);
+        if (len == -1) {
+            std::cerr << "Error receiving data\n";
+            return 1;
+        }
+        if (len == 0) {
+            std::cerr << "Client closed connection\n";
+            return 1;
+        }
+        std::cout << std::string(buf.data(), len);
+        if (buf.data()[len - 1] == '\0') {
+            std::string ack = "ACK";
+            send(connection, ack.data(), ack.length(), MSG_NOSIGNAL);
+            break;
+        }
+    }
+    close(connection);
+    close(server);
 
     return 0;
 }
